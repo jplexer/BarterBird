ea1715bd8debf751b55ad1b1070e4022ca93fa9e
JPlexer <joshuajun@proton.me>
2024-01-14 23:24:29 +0100

    Split queue into multple messages if too long

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -2,22 +2,45 @@ const { useMainPlayer } = require('discord-player');
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('queue')
-		.setDescription('view the queue'),
-	async execute(interaction, client) {
-		const player = useMainPlayer();
-		var queue = player.nodes.get(interaction.guildId);
-		if (!queue) {
-			return interaction.reply({ content:'You are not connected to a voice channel!', ephemeral: true });
-		}
+    data: new SlashCommandBuilder()
+        .setName('queue')
+        .setDescription('view the queue'),
+    async execute(interaction, client) {
+        const player = useMainPlayer();
+        var queue = player.nodes.get(interaction.guildId);
+        if (!queue) {
+            return interaction.reply({ content:'You are not connected to a voice channel!', ephemeral: true });
+        }
 
-		//return a list of songs in the queue as a code block
+        //return a list of songs in the queue as a code block
         var queueList = [];
         queueList.push("Now Playing: " + queue.currentTrack.title);
         for (var i = 0; i < queue.tracks.toArray().length; i++) {
             queueList.push((i + 1) + ". " + queue.tracks.toArray()[i].title);
         }
-        await interaction.reply({ content: "```" + queueList.join("\n") + "```" });
-	},
+
+        // Split the queueList into chunks of 2000 characters
+        var chunk = "";
+        var firstMessage = true;
+        for (var i = 0; i < queueList.length; i++) {
+            if ((chunk.length + queueList[i].length + 4) > 2000) { // +4 for "\n" and "```"
+                if (firstMessage) {
+                    await interaction.reply({ content: "```" + chunk + "```" });
+                    firstMessage = false;
+                } else {
+                    await interaction.followUp({ content: "```" + chunk + "```" });
+                }
+                chunk = queueList[i];
+            } else {
+                chunk += "\n" + queueList[i];
+            }
+        }
+        if (chunk.length > 0) {
+            if (firstMessage) {
+                await interaction.reply({ content: "```" + chunk + "```" });
+            } else {
+                await interaction.followUp({ content: "```" + chunk + "```" });
+            }
+        }
+    },
 };
