3d827eff83e2fe406728e3c4801859d6e20c1d91
Joshua Jun <joshuajun@proton.me>
2024-07-19 09:12:38 +0200

    Export Scrobbling and Now Playing into their own functions

--- index.js
+++ index.js
@@ -103,26 +103,8 @@ player.events.on('playerStart', async (queue, track) => {
     queue.metadata.channel.send(`Now Playing: **${track.title}** (${track.duration}) \n @ ${track.url}`);
 	if (lastfm) {
 		// Update last.fm now playing
-		var method = 'track.updateNowPlaying';
 		queue.channel.members.forEach(member => {
-			client.userconfig.findOne({ where: { userId: member.id } }).then(userconfig => {
-				if (userconfig) {
-					if (!userconfig.youtubeScrobble && track.source == 'youtube') {
-						return;
-					}
-					var session_key = userconfig.lastfmSessionKey;
-					var signature = crypto.createHash('md5').update(`api_key${lastfm_api_key}artist${track.author}method${method}sk${session_key}track${track.title}${lastfm_api_secret}`).digest('hex');
-					axios.post(`http://ws.audioscrobbler.com/2.0/?method=track.updateNowPlaying&api_key=${lastfm_api_key}&artist=${track.author}&track=${track.title}&sk=${session_key}&format=json&api_sig=${signature}`)
-					.catch(error => {
-						if (error.response.data.error == 9) {
-							// DM user to reauthenticate
-							member.send(`Your Last.fm session key is invalid. Please reauthenticate.`);
-							// delete their userconfig
-							client.userconfig.destroy({ where: { userId: member.id } });
-						}
-					});
-				}
-			});
+			setNowPlaying(track, member);
 		})
 	}
 });
@@ -135,33 +117,59 @@ player.events.on('playerFinish', async (queue, track) => {
 		}
 
 		//scrobble the track for each user in the channel
-		var method = 'track.scrobble';
 		queue.channel.members.forEach(member => {
-			client.userconfig.findOne({ where: { userId: member.id } }).then(userconfig => {
-				if (userconfig) {
-					if (!userconfig.youtubeScrobble && track.source == 'youtube') {
-						return;
-					}
-					var session_key = userconfig.lastfmSessionKey;
-					var timestamp = Math.floor(new Date().getTime() / 1000);
-					//var chosenByUser = member.id == queue.metadata.user.id ? 1 : 0;
-					var signature = crypto.createHash('md5').update(`api_key${lastfm_api_key}artist${track.author}method${method}sk${session_key}timestamp${timestamp}track${track.title}${lastfm_api_secret}`).digest('hex');
-					axios.post(`http://ws.audioscrobbler.com/2.0/?format=json&method=track.scrobble&api_key=${lastfm_api_key}&artist=${track.author}&track=${track.title}&timestamp=${timestamp}&sk=${session_key}&api_sig=${signature}`)
-					.then(response => {
-					})
-					.catch(error => {
-						if (error.response.data.error == 9) {
-							// DM user to reauthenticate
-							member.send(`Your Last.fm session key is invalid. Please reauthenticate.`);
-							// delete their userconfig
-							client.userconfig.destroy({ where: { userId: member.id } });
-						}
-					});
-				}
-			});
+			scrobbleSong(track, member);
 		})
 	}
 });
+
+function setNowPlaying(track, member) {
+	var method = 'track.updateNowPlaying';
+	client.userconfig.findOne({ where: { userId: member.id } }).then(userconfig => {
+		if (userconfig) {
+			if (!userconfig.youtubeScrobble && track.source == 'youtube') {
+				return;
+			}
+			var session_key = userconfig.lastfmSessionKey;
+			var signature = crypto.createHash('md5').update(`api_key${lastfm_api_key}artist${track.author}method${method}sk${session_key}track${track.title}${lastfm_api_secret}`).digest('hex');
+			axios.post(`http://ws.audioscrobbler.com/2.0/?method=track.updateNowPlaying&api_key=${lastfm_api_key}&artist=${track.author}&track=${track.title}&sk=${session_key}&format=json&api_sig=${signature}`)
+			.catch(error => {
+				if (error.response.data.error == 9) {
+					// DM user to reauthenticate
+					member.send(`Your Last.fm session key is invalid. Please reauthenticate.`);
+					// delete their userconfig
+					client.userconfig.destroy({ where: { userId: member.id } });
+				}
+			});
+		}
+	});
+}
+
+function scrobbleSong(track, member) {
+	var method = 'track.scrobble';
+	client.userconfig.findOne({ where: { userId: member.id } }).then(userconfig => {
+		if (userconfig) {
+			if (!userconfig.youtubeScrobble && track.source == 'youtube') {
+				return;
+			}
+			var session_key = userconfig.lastfmSessionKey;
+			var timestamp = Math.floor(new Date().getTime() / 1000);
+			//var chosenByUser = member.id == queue.metadata.user.id ? 1 : 0;
+			var signature = crypto.createHash('md5').update(`api_key${lastfm_api_key}artist${track.author}method${method}sk${session_key}timestamp${timestamp}track${track.title}${lastfm_api_secret}`).digest('hex');
+			axios.post(`http://ws.audioscrobbler.com/2.0/?format=json&method=track.scrobble&api_key=${lastfm_api_key}&artist=${track.author}&track=${track.title}&timestamp=${timestamp}&sk=${session_key}&api_sig=${signature}`)
+			.then(response => {
+			})
+			.catch(error => {
+				if (error.response.data.error == 9) {
+					// DM user to reauthenticate
+					member.send(`Your Last.fm session key is invalid. Please reauthenticate.`);
+					// delete their userconfig
+					client.userconfig.destroy({ where: { userId: member.id } });
+				}
+			});
+		}
+	});
+}
 
 
 // Login to Discord with your client's token
